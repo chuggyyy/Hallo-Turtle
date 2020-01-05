8b20594c262ad8e9bbe36c40d8cc0b08cccfbe30
chuggyyy <45316838+chuggyyy@users.noreply.github.com>
2020-01-05 13:41:12 +0100

    Create hallo.py

---
+++ hallo.py
@@ -0,0 +1,16 @@
+
+import turtle
+
+turtle.shape("turtle")
+turtle.left(45)
+turtle.forward(50)
+turtle.right(65)
+turtle.circle(150,350)
+turtle.home()
+big=("Arial", 36, "normal")
+turtle.penup()
+turtle.goto(-40,150)
+turtle.write("Hello World!", font=big)
+turtle.home()
+turtle.back(20)
+turtle.exitonclick()
